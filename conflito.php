e5361d478679c42e0df656c21d4c750e42325b76
unknown <jaciane@.fieb.org.br>
2013-08-01 09:03:18 -0300

    testando meu commit

--- conflito.php
+++ conflito.php
@@ -1,1 +1,9 @@
 <teste>teste de conflito mais outro teste</teste>asd
+<<<<<<< HEAD
+<?php 
+echo "teste conflito";
+ ?>
+=======
+
+<?php echo "jaci";	?>
+>>>>>>> 963279aabf0235939ec8511cc4b1cd4a9a2233a8

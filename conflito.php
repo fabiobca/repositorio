0be4f25d06d25586b5f7ec993896fe5762f97a57
unknown <jaciane@.fieb.org.br>
2013-08-01 09:41:50 -0300

    fewtttttew

--- conflito.php
+++ conflito.php
@@ -1,9 +1,2 @@
-<teste>teste de conflito mais outro teste</teste>asd
-<<<<<<< HEAD
-<?php 
-echo "teste conflito";
- ?>
-=======
+<?php echo "meu  teste jaci...";	?>
 
-<?php echo "jaci";	?>
->>>>>>> 963279aabf0235939ec8511cc4b1cd4a9a2233a8
